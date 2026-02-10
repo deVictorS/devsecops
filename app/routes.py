6581bb4abfce62d42f5b9fa4d3a1768f74b19a71
deVictorS <victorsantos3245@gmail.com>
2026-02-10 15:12:30 -0300

    Start

---
+++ app/routes.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, request
+
+main = Blueprint("main", __name__)
+
+@main.route("/")
+def home():
+    return render_template("index.html")
+
+@main.route("/processar", methods=["POST"])
+def processar():
+    nome = request.form.get("nome")
+    return render_template("resultado.html", nome=nome)
