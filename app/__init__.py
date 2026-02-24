123c25a619204ffa9bb56e9ca63eb60f0e1ac7b2
deVictorS <victorsantos3245@gmail.com>
2026-02-24 10:27:39 -0300

    teste

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from .routes import main
-    app.register_blueprint(main)
+    import routes
+    app.register_blueprint(routes.main)
 
     return app
