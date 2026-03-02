4b451fb93c8060aa114a6fc12bb6a5960ba074d8
deVictorS <victorsantos3245@gmail.com>
2026-03-02 15:25:25 -0300

    att versao docker

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    import app.app.routes as routes
+    from . import routes
     app.register_blueprint(routes.main)
 
     return app
