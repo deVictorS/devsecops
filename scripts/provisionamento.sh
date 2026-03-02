cd306a883089dfae7767f58a56a831fef2eb3109
deVictorS <victorsantos3245@gmail.com>
2026-03-02 07:54:02 -0300

    Teste

--- scripts/provisionamento.sh
+++ scripts/provisionamento.sh
@@ -2,11 +2,12 @@ echo "Iniciando provisionamento com Ansible. . ."
 
 cd ../terraform
 CHAVE="../projeto-devsecops.pem"
+chmod 400 "$CHAVE"
 
-IP_VM=$(terraform output -raw ip_pubico | tr -d '\r\n')
+IP_VM=$(terraform output -raw ip_publico | tr -d '\r\n')
 
-ansible-playbook -i "$IP_VM," -u ubuntu --private-key CHAVE \
+ansible-playbook -i "$IP_VM," -u ubuntu --private-key "$CHAVE" \
     --ssh-common-args='-o StrictHostKeyChecking=no' \
-    ../ansible/playbook.yml
+    ../ansible/provisionamento.yaml
 
 echo "Provisionamento concluído!"
